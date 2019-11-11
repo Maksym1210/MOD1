c0de989d25d68032b4ef7e2ef7f248ac7c75f195
Max1210 <maksimgnatisko@gmail.com>
2019-11-11 16:33:46 +0200

    added classes Bird, Sparrow and Nightingale

--- main.cpp
+++ main.cpp
@@ -2,7 +2,51 @@
 
 using namespace std;
 
+class Bird
+{
+protected:
+    char* song ;
+    virtual void sing() = 0;
+};
+
+class Nightingale : public Bird
+{
+public:
+    Nightingale(char* a)
+    {
+        song = a;
+    }
+    void sing()
+    {
+        cout << song << endl;
+    }
+
+};
+
+class Sparrow : public Bird
+{
+public:
+    Sparrow(char* a)
+    {
+        song = a;
+    }
+    void sing()
+    {
+        int i = 0;
+        while(i<3)
+        {
+            cout << song[i] ;
+            i++;
+        }
+        cout << endl;
+    }
+};
+
 int main()
 {
+    Nightingale a("alalalalalong");
+    Sparrow b("alalalalalong");
+    a.sing();
+    b.sing();
     return 0;
 }
