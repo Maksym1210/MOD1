2c838e5e57a46d451eddeb2c23476d63788f0447
Max1210 <maksimgnatisko@gmail.com>
2019-11-11 18:06:24 +0200

    added SpellChecker class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
@@ -42,11 +44,37 @@ public:
     }
 };
 
+class SpellChecker
+{
+public:
+    vector <char*> a = { "aa", "bbb", "cccc" };
+    char* method(char* word, int &dif)
+    {
+        int coincidences[a.size()] = {};
+        for(size_t j = 0; j < a.size(); j++)
+        for(size_t i = 0; i < min(strlen(word), strlen(a[j])); i++)
+        {
+            if(word[i] == a[j][i])coincidences[j]++;
+        }
+        int ind_max = 0;
+        for(size_t i = 1; i < a.size(); i++)
+        {
+            if(coincidences[i] > coincidences[i-1])ind_max = i;
+        }
+        dif = (max(strlen(word), strlen(a[ind_max])) - coincidences[ind_max]);
+        return a[ind_max];
+    }
+};
+
 int main()
 {
     Nightingale a("alalalalalong");
     Sparrow b("alalalalalong");
     a.sing();
     b.sing();
+    SpellChecker c;
+    int dif;
+    cout << c.method("bcac", dif) << endl;
+    cout << "differences : " << dif << endl;
     return 0;
 }
